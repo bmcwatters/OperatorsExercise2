c5a19b4010415dc71e866fc4353c08bd53eb4118
bryan mcwatters <bmcwatters@comcast.net>
2021-12-04 16:25:58 -0500

    initial commit

---
+++ OperatorsExercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+    Console.WriteLine("What is the radius of your circle?");
+    var radius = double.Parse(Console.ReadLine());
+    
+
+    double result = AreaOfCircle(radius);
+    Console.WriteLine($"The area of a circle with radius of {radius} is {result}");
+
+
+
+static double AreaOfCircle(double radius)
+{
+       
+    return Math.PI * radius * radius;
+
+}
+
